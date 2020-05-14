e2092c9897c61b92b6065ab45e0dee470b3cf7d7
PatrickYang <azureroki@gmail.com>
2020-05-14 12:38:52 +0800

    Updates

---
+++ precache-manifest.e7a17708bffc3e12d5c82a9d984e214e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376a94bf8a891149b42fcd60e8a59afa",
+    "url": "/React-CRUD-Auth/index.html"
+  },
+  {
+    "revision": "e08198e454cff351d61e",
+    "url": "/React-CRUD-Auth/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "c60a89549bd46336a3b1",
+    "url": "/React-CRUD-Auth/static/js/2.aee78fee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-CRUD-Auth/static/js/2.aee78fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e08198e454cff351d61e",
+    "url": "/React-CRUD-Auth/static/js/main.daa5513c.chunk.js"
+  },
+  {
+    "revision": "b166b0b8a36dc1468f36",
+    "url": "/React-CRUD-Auth/static/js/runtime-main.65857670.js"
+  }
+]);
